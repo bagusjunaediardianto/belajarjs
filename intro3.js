ac480aed052181bbc0b92cd330d35182bbbf4929
bagusjunaediardianto <bagusardianto137@gmail.com>
2018-12-01 01:06:12 +0700

    menambahkan file intr3.js

---
+++ intro3.js
@@ -0,0 +1,15 @@
+var pesan1 = "hello"
+let pesan2 = "coding"
+const pesan3 = "hello coding"
+
+if (true){
+  var pesan1 = 1
+  let pesan2 = 2
+  const pesan3 = 3
+
+  console.log(pesan2)
+  console.log(pesan3)
+}
+console.log(pesan1)
+console.log(pesan2)
+console.log(pesan3)
