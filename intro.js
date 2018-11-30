48041763cbb5460fa0d5d94e52d499d046b7a106
bagusjunaediardianto <bagusardianto137@gmail.com>
2018-12-01 00:51:42 +0700

    menambahkan file intro.js

---
+++ intro.js
@@ -0,0 +1,13 @@
+var pesan1 = "hello coding"
+let pesan2 = "hello"
+let pesan3 = "coding"
+
+
+//memasukkan value baru pada masing-masing variabel
+pesan1 = "change words"
+pesan2 = "change words"
+//variabel const tidak dapat berubah valuenya / non imuttable
+pesan3 = "change words"
+console.log(pesan1)
+console.log(pesan2)
+console.log(pesan3)
