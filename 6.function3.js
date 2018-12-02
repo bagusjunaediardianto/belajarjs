bfb998220e524f7af82e159576d5165af9625e01
bagusjunaediardianto <bagusardianto137@gmail.com>
2018-12-02 22:37:21 +0700

    menambahkan file 6.function3.js && file 7.if-else.js

---
+++ 6.function3.js
@@ -0,0 +1,18 @@
+var panggilNilai = ()=>{
+  var nilai = 3
+
+  if (nilai === 3) console.log("Benar")
+  else console.log("salah")
+}
+
+function panggilNilai1(){
+  var nilai = 3
+
+  if (nilai === 3){
+    console.log("Benar")
+  }
+  else{
+    console.log("Salah")
+  }
+}
+panggilNilai()
