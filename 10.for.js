e31aca3fb8260ab5f9a7956fce7318719f10f5d4
bagusjunaediardianto <bagusardianto137@gmail.com>
2018-12-03 14:53:21 +0700

    menambahkan file 10.for.js

---
+++ 10.for.js
@@ -0,0 +1,17 @@
+var panggilFor = ()=>{
+  var i = 0
+  var data = [10]
+
+  for (i = 1; i <= data[0]; i++){
+    if (i % 3 === 0)
+    {
+      console.log("Hai")
+    }
+    else{
+      console.log(i)
+    }
+
+  }
+}
+
+panggilFor()
