25535bc712823d72e07eec02bc341b9d72ea3764
bagusjunaediardianto <bagusardianto137@gmail.com>
2018-12-30 02:48:42 +0700

    menambahkan 32.map.js

--- 32.map.js
+++ 32.map.js
@@ -12,3 +12,18 @@ function panggilMap(){
 }
 
 panggilMap()
+console.log("------------------------------------")
+var panggilMap1 = function(){
+  var data = {
+    kota : ["jakarta","bandung","surabaya"]
+  }
+  var datakota = data.kota
+
+  datakota.map(function(item,index,array){
+    console.log(item)
+    console.log(index)
+    console.log(array)
+  })
+}
+
+panggilMap1()
